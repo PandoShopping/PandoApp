d9f17aff03313bcc3959e45193115cf5daa7feef
Asritha Bodepudi <azb0222@dexcom.com>
2023-02-24 16:20:15 -0500

    asdfasdF

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View} from "react-native";
 import { CurrentRenderContext, NavigationContainer } from '@react-navigation/native';
+// import {
+//   createMaterialBottomTabNavigator,
+//   NavigationMaterialBottomTabOptions,
+// } from 'react-navigation-material-bottom-tabs';
 
- import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
  //import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { BottomNavigation, Text, DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import React, { useState, useEffect, Component } from 'react';
 
-//import {PaperProvider} from 'react-native-paper';
+
 
 import { ProfileHomeScreen, AddListingScreen, BrowseListingsScreen} from "./screens"
 import Icon from 'react-native-vector-icons/FontAwesome5'
@@ -39,47 +42,86 @@ const theme = {
   },
 };
 
-const Tab = createBottomTabNavigator();
+
+
+
 
 function MyTabs() {
+  // return (
+  //  // <PaperProvider theme={DefaultTheme}>
+
+  // //  </PaperProvider>
+
+
+  
+  
+  // // <Tab.Navigator
+  // //   initialRouteName="Shop"
+  // //   barStyle={{ backgroundColor: 'tomato' }}
+
+  // //   activeColor="#e91e63"
+
+  // //   screenOptions={{
+  // //     tabBarActiveTintColor: COLORS.buttonPrimaryMedium,
+  // //     backgroundColor: 'green'
+  // //   }}
+
+  // // >
+
+  // //     <Tab.Screen
+  // //       name="Shop"
+  // //       component={BrowseListingsScreen}
+  // //       options={{
+  // //         tabBarLabel: 'Shop',
+  // //         tabBarIcon: ({ color }) => (
+  // //           <MaterialCommunityIcons name="home" color={color} size={26} />
+  // //         ),
+  // //       }}
+  // //     />
+
+
+  // //    <Tab.Screen name="Sell" component={AddListingScreen} />
+  // //    <Tab.Screen name="Profile" component={ProfileHomeScreen} />
+  // //   </Tab.Navigator>
+  // <BottomNavigation
+  //     navigationState={{ index, routes }}
+  //     onIndexChange={setIndex}
+  //     renderScene={renderScene}
+  //   />
+
+
+  // );
+
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    { key: 'music', title: 'Favorites', focusedIcon: 'heart', unfocusedIcon: 'heart-outline', backgroundColor: 'green'},
+    { key: 'albums', title: 'Albums', focusedIcon: 'album', color: 'green'},
+    { key: 'recents', title: 'Recents', focusedIcon: 'history', color: 'green' },
+    { key: 'notifications', title: 'Notifications', focusedIcon: 'bell', unfocusedIcon: 'bell-outline', color: 'green' },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    music: BrowseListingsScreen,
+    albums: AddListingScreen,
+    recents: AddListingScreen,
+    notifications: ProfileHomeScreen,
+  });
+
+  // const renderTab = (route, focused, color) => { 
+  //   switch (route.key) {
+  //     case 'music':
+  //       return <MusicRoute jumpTo={jumpTo} color={color} focused={focused} />;
+  //     case 'albums':
+  //       return <AlbumsRoute jumpTo={jumpTo} />;
+  //   }
+  // }
+
   return (
-   // <PaperProvider theme={DefaultTheme}>
-
-  //  </PaperProvider>
-
-
-  
-  
-  <Tab.Navigator
-    initialRouteName="Shop"
-    barStyle={{ backgroundColor: 'tomato' }}
-
-    activeColor="#e91e63"
-
-    screenOptions={{
-      tabBarActiveTintColor: COLORS.buttonPrimaryMedium,
-      backgroundColor: 'green'
-    }}
-
-  >
-
-      <Tab.Screen
-        name="Shop"
-        component={BrowseListingsScreen}
-        options={{
-          tabBarLabel: 'Shop',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
-
-
-     <Tab.Screen name="Sell" component={AddListingScreen} />
-     <Tab.Screen name="Profile" component={ProfileHomeScreen} />
-    </Tab.Navigator>
-
-
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
   );
 }
 
--- screens/shop/BrowseListingsScreen.js
+++ screens/shop/BrowseListingsScreen.js
@@ -1,25 +1,18 @@
 import React from "react";
-import { Text, View, StyleSheet} from "react-native";
+import { Text, View, StyleSheet, ScrollView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import ListingsViewer from "./../../components/ListingsViewer"
 import Button from './../../components/Button'
-//import Listing from "./../../components/Listing"
-//import ListingsViewer from "./../../components/ListingsViewer";
 
 export const BrowseListingsScreen = () => {
   
-return (
-	<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-    <ListingsViewer/>
-	</View>
-);
- };
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView>
+        <ListingsViewer />
+      </ScrollView>
+    </View>
+  );
+};
 
- const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
- 
 export default BrowseListingsScreen;
