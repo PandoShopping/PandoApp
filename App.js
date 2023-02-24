6527ed7595fe551607cffee7aa0995473b30f77c
Vidushi <meelvidushi1@gmail.com>
2023-02-24 15:02:45 -0600

    final

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View} from "react-native";
 import { CurrentRenderContext, NavigationContainer } from '@react-navigation/native';
-
- import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
- //import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-
-//import {PaperProvider} from 'react-native-paper';
-
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
 import { ProfileHomeScreen, AddListingScreen, BrowseListingsScreen} from "./screens"
 import Icon from 'react-native-vector-icons/FontAwesome5'
 import { Amplify } from 'aws-amplify'
@@ -17,9 +9,10 @@ import awsmobile from './src/aws-exports'
 import { withAuthenticator, SignIn, ConfirmSignUp, ConfirmSignIn, ForgotPassword } from 'aws-amplify-react-native';
 import { AmplifyTheme } from 'aws-amplify-react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-import { BlurView } from 'expo-blur';
-
 import { COLORS } from "./constants";
+
+
+
 Amplify.configure({
   ...awsmobile,
   Analytics: {
@@ -27,59 +20,44 @@ Amplify.configure({
   }
 });
 
-
-
-const theme = {
-  ...DefaultTheme,
-  roundness: 2,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'tomato' ,
-    accent: 'tomato' ,
-  },
-};
-
-const Tab = createBottomTabNavigator();
+const Tab = createMaterialBottomTabNavigator();
 
 function MyTabs() {
   return (
-   // <PaperProvider theme={DefaultTheme}>
+    // <Tab.Navigator 
+    // >
+    //   <Tab.Screen name="Shop" component={BrowseListingsScreen} />
+    //   <Tab.Screen name="Sell" component={AddListingScreen} />
+    //   <Tab.Screen name="Profile" component={ProfileHomeScreen} />
+    // </Tab.Navigator>
 
-  //  </PaperProvider>
+    <Tab.Navigator initialRouteName="Sell"
+    activeColor="#156FD7"
+  inactiveColor="gray"
+    barStyle={{ backgroundColor: 'white', }}
 
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
 
-  
-  
-  <Tab.Navigator
-    initialRouteName="Shop"
-    barStyle={{ backgroundColor: 'tomato' }}
+        if (route.name === 'Shop') {
+          iconName = focused
+            ? 'ios-information-circle'
+            : 'ios-information-circle-outline';
+        } else if (route.name === 'Sell') {
+          iconName = focused ? 'ios-list' : 'ios-list-outline';
+        } else if (route.name === 'Profile')
+          iconName = focused ? 'ios-list' : 'ios-list-outline';
 
-    activeColor="#e91e63"
-
-    screenOptions={{
-      tabBarActiveTintColor: COLORS.buttonPrimaryMedium,
-      backgroundColor: 'green'
-    }}
-
+        return <Ionicons name={iconName} size={20} color={color} />;
+      },
+      
+    })}
   >
-
-      <Tab.Screen
-        name="Shop"
-        component={BrowseListingsScreen}
-        options={{
-          tabBarLabel: 'Shop',
-          tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
-          ),
-        }}
-      />
-
-
-     <Tab.Screen name="Sell" component={AddListingScreen} />
-     <Tab.Screen name="Profile" component={ProfileHomeScreen} />
-    </Tab.Navigator>
-
-
+    <Tab.Screen name="Shop" component={BrowseListingsScreen} />
+    <Tab.Screen name="Sell" component={AddListingScreen} />
+    <Tab.Screen name="Profile" component={ProfileHomeScreen} />
+  </Tab.Navigator>
   );
 }
 
@@ -98,17 +76,12 @@ const MyAppHeader = () => {
 
 
 function App() {
-  return ( 
-     
-    <PaperProvider  theme={theme}>
-<NavigationContainer theme = {customTheme}>
-  {/* <StatusBar style="auto" /> */}
-  <MyTabs />
-
+  return (  
+   <NavigationContainer theme = {customTheme}>
+   <MyTabs />
+  <StatusBar style="auto" />
+ 
  </NavigationContainer>
-
-    </PaperProvider>
-   
    );
  }
 
@@ -118,61 +91,6 @@ const customTheme = {
     ...AmplifyTheme.colors,
     background: 'white'
   },
-  hereig: {
-    padding: 10, 
-    backgroundColor: 'green'
-  }
 };
 
-
-
- export default withAuthenticator(App, false, [], null, customTheme);
-
-
- // <Tab.Navigator 
-    // >
-    //   <Tab.Screen name="Shop" component={BrowseListingsScreen} />
-    //   <Tab.Screen name="Sell" component={AddListingScreen} />
-    //   <Tab.Screen name="Profile" component={ProfileHomeScreen} />
-    // </Tab.Navigator>
-
-    // <Tab.Navigator initialRouteName="Sell"
-    // activeColor="#156FD7"
-    // inactiveColor="gray"
-    // barStyle={{ backgroundColor: 'white' }}
-    // style={{ backgroundColor: 'tomato' }}
-
-    
-
-  //   screenOptions={
-  //     tabBarActiveBackgroundColor: 'green',
-  //     // ({ route }) => ({
-
-        
-   
-  //     // tabBarIcon: ({ focused, color, size }) => {
-        
-  //     //   let iconName;
-
-        
-
-  //     //   if (route.name === 'Shop') {
-  //     //     iconName = focused
-  //     //       ? 'ios-information-circle'
-  //     //       : 'ios-information-circle-outline';
-  //     //   } else if (route.name === 'Sell') {
-  //     //     iconName = focused ? 'ios-list' : 'ios-list-outline';
-  //     //   } else if (route.name === 'Profile')
-  //     //     iconName = focused ? 'ios-list' : 'ios-list-outline';
-
-  //     //   return <Ionicons name={iconName} size={20} color={color} />;
-  //     // },
-
-      
-  //  // })
-  // // }
-  // >
-  //   <Tab.Screen name="Shop" component={BrowseListingsScreen}  />
-  //   <Tab.Screen name="Sell" component={AddListingScreen} />
-  //   <Tab.Screen name="Profile" component={ProfileHomeScreen} />
-  // </Tab.Navigator>
+export default withAuthenticator(App, false, [], null, customTheme);
